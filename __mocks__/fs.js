b04425b4a0e226895c7798b34b23b3765a2a5bac
jin1995 <897073574@qq.com>
2019-10-17 17:37:34 +0800

    test & mock

---
+++ __mocks__/fs.js
@@ -0,0 +1,22 @@
+const fs = jest.genMockFromModule('fs')
+const _fs = jest.requireActual('fs')
+
+Object.assign(fs, _fs)
+
+const mocks = {}
+
+fs.setMock = (path,error,data) => {
+  mocks[path] = [error, data]
+}
+
+fs.readFile = (path, options, callback) => {
+  if(callback === undefined){callback = options}
+  if(path in mocks){
+    callback(...mocks[path])
+  }else{
+    console.log('mock');
+    // _fs.readFile(path, options, callback)
+  }
+}
+
+module.exports = fs
