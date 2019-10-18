4a889022d4b8e4b11ac541f85e6dcc027a0c341c
jin1995 <897073574@qq.com>
2019-10-18 11:54:34 +0800

    writeMock test

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -3,20 +3,40 @@ const _fs = jest.requireActual('fs')
 
 Object.assign(fs, _fs)
 
-const mocks = {}
+let readMocks = {}
 
-fs.setMock = (path,error,data) => {
-  mocks[path] = [error, data]
+fs.setReadFileMock = (path, error, data) => {
+  readMocks[path] = [error, data]
 }
 
 fs.readFile = (path, options, callback) => {
-  if(callback === undefined){callback = options}
-  if(path in mocks){
-    callback(...mocks[path])
-  }else{
-    console.log('mock');
+  if (callback === undefined) { callback = options }
+  if (path in readMocks) {
+    callback(...readMocks[path])
+  } else {
+    console.log('readMock');
     // _fs.readFile(path, options, callback)
   }
 }
+
+let writeMocks = {}
+
+fs.setWriteFileMock = (path, fn) => {
+  writeMocks[path] = fn
+}
+fs.writeFile = (path, data, options, callback) => {
+  if(path in writeMocks){
+    writeMocks[path](path,data,options,callback)
+  }else{
+    console.log('writeMock');
+    // _fs.writeFile(file,data,options,callback)
+  }
+}
+
+fs.clearMocks = ()=>{
+  readMocks = {}
+  writeMocks = {}
+}
+
 
 module.exports = fs
--- __tests__/db.spec.js
+++ __tests__/db.spec.js
@@ -3,11 +3,26 @@ const fs = require('fs')
 
 jest.mock('fs')
 
-describe('db', ()=>{
+describe('db', () => {
+  afterEach(()=>{
+    fs.clearMocks()
+  })
   it('can read', async () => {
-    const data = [{title: 'hi', done: true}]
-    fs.setMock('./xxx', null, JSON.stringify(data))
+    const data = [{ title: 'hi', done: true }]
+    fs.setReadFileMock('./xxx', null, JSON.stringify(data))
     const list = await db.read('./xxx')
     expect(list).toStrictEqual(data)
+  })
+  it('can write', async () => {
+    let fakeFile
+    fs.setWriteFileMock('./yyy', (path, data, callback) => {
+      fakeFile = data
+      callback()
+    })
+    
+    const list = [{ title: 'fake', done: true }]
+    
+    await db.write(list, './yyy')
+    expect(fakeFile).toBe(JSON.stringify(list) + '\n')
   })
 })
